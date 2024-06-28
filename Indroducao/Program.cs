92945a48c46f28a6a1e64307cd990d222f872d25
danilocalixto77 <danilocalixto@gmail.com>
2024-06-27 23:01:27 -0400

    study: 20240627 2300

---
+++ Indroducao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach (int i in numeros.Where(n => n > 4)) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@
 # Dominando LINQ e Lambda Expressions com C#
 **Professor: Paulo Rogério Martins Marques - Udemy**
 
+## Seção 1: Introdução
+
+### 1.Introdução
+
+    O LINQ possibilita filtrar de maneira simples, dentro da própria experessão utilizando de uma sintaxe muito próxima do SQL e utilizando expressões Lambdas para simplificar. Devolvendo uma melhor performance em estruturas diversas.
