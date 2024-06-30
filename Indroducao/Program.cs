7ec8fca35e37fcc5efba253cd0389a33bd8d852a
danilocalixto77 <danilocalixto@gmail.com>
2024-06-30 08:12:29 -0400

    study: 202406300812

--- Indroducao/Program.cs
+++ Indroducao/Program.cs
@@ -8,14 +8,37 @@ namespace Introducao {
     internal class Program {
         static void Main(string[] args) {
 
-            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] numeros = { 1, 6, 7, 8, 9, 2, 3, 4, 5,  10, 5, 6 };
 
 
-            var resultado = from num in numeros select num;
+            var resultado = from num in numeros
+                            where num >= 4
+                            orderby num 
+                            select num;
 
-            foreach (int i in numeros.Where(n => n > 4)) {
-                Console.WriteLine(i);
+            foreach (var r in resultado) {
+                Console.WriteLine("Descrevendo query {0}", r);
             }
+
+
+            var resultadoDois = numeros.Where(n => n > 4).OrderBy(o => o);
+            
+
+                foreach (int r2 in resultadoDois) {
+                Console.WriteLine(r2);
+            }
+
+
+
+            string[] cores = { "Preto", "Branco", "Verde", "Vermelho", "Azul" };
+
+            var resultadoCores = cores.Where(x => x.Contains("e") || x.Contains("z"));
+
+            foreach( var rC1 in resultadoCores) {
+                Console.WriteLine(rC1);
+            }
+
+
         }
     }
 }
