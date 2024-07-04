9f5adeb60c6bea1e0f45b5957f31db4f69cf763c
danilocalixto77 <danilocalixto@gmail.com>
2024-07-04 07:12:24 -0400

    202407040712

--- Loja/Program.cs
+++ Loja/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Loja.Dominio.Entidades;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,20 +20,30 @@ namespace Loja {
             //Console.ReadKey();
 
             //Uma forma reduzida
-            new Produto().Listar().ForEach(item => Console.WriteLine(item.Nome));
+            //new Produto().Listar().ForEach(item => Console.WriteLine(item.Nome));
 
             //Concatenando atributos do objeto
-            new Produto().Listar().ForEach(item => 
-            {  Console.WriteLine(item.Nome); 
-               Console.WriteLine(item.Valor);
-            });
+            //new Produto().Listar().ForEach(item => {
+            //    Console.WriteLine(item.Nome);
+            //    Console.WriteLine(item.Valor);
+            //});
 
 
-            //Pare este exemplo é necessário formatar para o tipo List o foreach
-            new Produto().Listar().Where(p => p.Valor >4).ToList().ForEach(item => {
-                Console.WriteLine(item.Nome);
-                Console.WriteLine(item.Valor);
-            });
+            ////Pare este exemplo é necessário formatar para o tipo List o foreach
+            //new Produto().Listar().Where(p => p.Valor > 4).ToList().ForEach(item => {
+            //    Console.WriteLine(item.Nome);
+            //    Console.WriteLine(item.Valor);
+            //});
+
+
+            //Usando JSon 
+            var produtos = new Produto().Listar();
+            produtos = produtos.Where(p => p.Nome.Contains("B")).ToList();
+            produtos.ForEach(item => Console.WriteLine(JsonConvert.SerializeObject(item)));
+            Console.ReadKey();
+                
+
+
 
 
         }
