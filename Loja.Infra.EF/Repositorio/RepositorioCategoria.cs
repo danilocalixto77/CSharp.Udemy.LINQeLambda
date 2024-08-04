ec6e30a94ccd56895f755ffd149d83b55775f926
danilocalixto77 <danilocalixto@gmail.com>
2024-08-04 08:26:41 -0400

    202408040826

---
+++ Loja.Infra.EF/Repositorio/RepositorioCategoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Infra.EF.Repositorio {
+    public class RepositorioCategoria {
+        public void AdicionarCategoria(string nome) {
+            Categoria catFrutas = new Categoria();
+            catFrutas.nome = "Frutas";
+
+            Categoria catEletronicos = new Categoria();
+            catEletronicos.nome = "Eletronicos";
+
+            LojaEFEntities ef = new LojaEFEntities();
+
+            ef.Categoria.Add(catFrutas);
+            ef.Categoria.Add(catEletronicos);
+
+
+    }
+}
