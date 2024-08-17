4ad407e624a23b46b17ded1ae0338a785c2d67cc
danilocalixto77 <danilocalixto@gmail.com>
2024-08-17 08:30:09 -0400

    novo projeto com compatibildiade .net

---
+++ Aula08/Loja8.Infra.EF/Repositorio/RepositorioCategoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Loja8.Infra.EF.Repositorio {
+    public class RepositorioCategoria {
+        public void AdicionarCategoria(string nome) {
+
+            Categoria cat = new Categoria();
+
+            cat.nome = nome;
+
+            Loja8EFEntities ef = new Loja8EFEntities();
+
+            ef.Categoria.Add(cat);
+
+            ef.SaveChanges();
+
+
+
+        }
+    }
+}
