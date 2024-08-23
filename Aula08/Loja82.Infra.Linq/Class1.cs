ec41e766801ed8643113be101c543183bf92491a
danilocalixto77 <danilocalixto@gmail.com>
2024-08-22 23:54:32 -0400

    202408222354

---
+++ Aula08/Loja82.Infra.Linq/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja82.Infra.Linq
+{
+    public class Class1
+    {
+    }
+}
---
+++ Aula08/Loja82/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja82 {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
